78931c4054c62ca4526ccd1391644b0bcc5aa0d4
Daniel Borisenko <danielborisenko@Dans-MacBook-Pro.local>
2020-03-04 14:15:51 +0200

    run eject

---
+++ marketplace1/src/MarketStore.js
@@ -0,0 +1,10 @@
+import {observable} from 'mobx'
+import 
+class MarketStore{
+    @decorator products =[];
+    @decorator a = "";
+}
+
+var store = window.store = new MarketStore;
+
+export default store;
